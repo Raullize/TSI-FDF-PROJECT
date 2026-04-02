3b676fea43c4413488b7b63741a5c116cc6c2fb1
Raul Lize Teixeira <lizeraul1@gmail.com>
2026-04-02 13:27:11 -0300

    test(cart): add mock data for cart items
    Add mock data for cart items to facilitate testing and development of cart-related components.

---
+++ src/components/cart/mock.ts
@@ -0,0 +1,27 @@
+export const MOCK_CART_ITEMS = [
+  {
+    id: 101,
+    name: "Páprica Defumada",
+    price: 4.50,
+    quantity: 2,
+    image: "/images/products/noImage.png",
+    unit: "100g"
+  },
+  {
+    id: 102,
+    name: "Açafrão Puro",
+    price: 5.00,
+    promotionalPrice: 4.20,
+    quantity: 1,
+    image: "/images/products/noImage.png",
+    unit: "pacote 500g"
+  },
+  {
+    id: 601,
+    name: "Castanha de Caju W1",
+    price: 13.50,
+    quantity: 3,
+    image: "/images/products/noImage.png",
+    unit: "100g"
+  }
+];
